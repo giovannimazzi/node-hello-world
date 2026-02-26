9fca927626e0c1e70317dcd506839befc115448c
giovannimazzi <gioelor@libero.it>
2026-02-26 17:00:35 +0100

    add code examples in server.js

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-console.log(`Hello Boolean`);
+const fs = require("fs");
+
+const packageJson = JSON.parse(fs.readFileSync("package.json", "utf-8"));
+const authorInfo = packageJson.author.split("<");
+
+console.log(`
+    
+Hello Booleaner: ${process.argv[2]}
+
+The author is: ${authorInfo[0]}
+and his email is: "${authorInfo[1].slice(0, -1)}"
+
+`);
